0e230abf5d3c6a56ebeac8198263b7e09139146c
Musix <ljw927@naver.com>
2022-09-22 23:02:21 +0900

    useEffect & cleanup function

--- nomad_reactjs/nomad-react/src/App.js
+++ nomad_reactjs/nomad-react/src/App.js
@@ -1,11 +1,24 @@
 import Button from "./js/Button";
 import styles from "./css/App.module.css";
+import {useState, useEffect} from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("I'm here");
+    return () => console.log("I'm gone");
+  }, [])
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text={"Continue"}/>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
