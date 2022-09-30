1b335de059d1e89e73aa3b93f8dde0fe3f41bc4e
Musix <ljw927@naver.com>
2022-09-30 23:41:01 +0900

    using routers from react-router-dom

--- nomad_reactjs/nomad-react/src/App.js
+++ nomad_reactjs/nomad-react/src/App.js
@@ -2,9 +2,21 @@ import Button from "./js/Button";
 import styles from "./css/App.module.css";
 import {useState, useEffect} from "react";
 import Movie from "./js/Movie";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/movie" element={<Detail />}/>
+    </Routes>
+  </Router>;
 }
 
 export default App;
