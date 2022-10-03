83a40a43753681cdf02483dfc9c66caca1f6884d
Musix <ljw927@naver.com>
2022-10-03 10:57:19 +0900

    text limits & styles work(1)

--- nomad_reactjs/nomad-react/src/js/Movie.js
+++ nomad_reactjs/nomad-react/src/js/Movie.js
@@ -1,19 +1,17 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import styles from "../css/Movie.module.css"
 
 const Movie = ({id, coverImg, title, summary, genres}) => {
     return (
-        <div>
-            <img src={coverImg} alt={title}/>
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>
-            </h2>
-            <p>{summary}</p>
-            <ul>
-                {genres.map((g) =>(
-                <li key={g}>{g}</li>
-                ))}
-            </ul>
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title} className={styles.img} />
+            <div className={styles.detail}>
+                <h2>
+                    <Link to={`/movie/${id}`}  className={styles.h2}>{title}</Link>
+                </h2>
+                <p>{summary.length > 150 ? `${summary.slice(0, 150)}...` : summary}</p>
+            </div>
         </div>
     );
 };
