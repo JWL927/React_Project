48df07aa8fe1a88a268cca4b9d427f25b068663a
Musix <ljw927@naver.com>
2022-10-05 22:39:04 +0900

    design change

--- nomad_reactjs/nomad-react/src/routes/Home.js
+++ nomad_reactjs/nomad-react/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../js/Movie";
+import styles from "../css/Home.module.css"
 
 const Home = () => {
     const [loading, setLoading] = useState(true);
@@ -18,7 +19,7 @@ const Home = () => {
             {loading ? (
                 <h1>Loading...</h1>
             ) : (
-                <div>
+                <div className={styles.container}>
                     {movies.map((movie) => (
                         <Movie 
                             key={movie.id}
